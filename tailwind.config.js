cc76087b8449ca6d50ff2ce9e1c8828e27bb0b80
Ferrian Septiawan <ferrian27@gmail.com>
2020-11-20 23:41:08 +0700

    baru sampe navigasi special to try

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        'display': ['Quicksand']
+      },
+      colors: {
+        'purple-dark': '#272042',
+      },
+      fontSize: {
+        '2.5xl': ['1.75rem'],
+        '4.8xl': ['2.875rem'],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
