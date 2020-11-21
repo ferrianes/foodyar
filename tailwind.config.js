d34a90443acc51c8cda9897e1b48df9bb2e7fe8a
Ferrian Septiawan <ferrian27@gmail.com>
2020-11-21 17:18:11 +0700

    done but not responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,15 @@ module.exports = {
       fontSize: {
         '2.5xl': ['1.75rem'],
         '4.8xl': ['2.875rem'],
+        'testimonial': ['550px', '600px'],
       },
+      margin: {
+        '25': '100px',
+        '38': '150px',
+      },
+      padding: {
+        '18': '70px',
+      }
     },
   },
   variants: {
